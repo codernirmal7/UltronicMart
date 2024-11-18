ada659ac9c123af88ef9fb5dfcf05000752fcf4a
Nirmal Bam <codernirmalbam@gmail.com>
2024-11-18 14:33:53 +0545

    Add bg color white

--- frontend/src/components/Navbar/LogoNavbar.tsx
+++ frontend/src/components/Navbar/LogoNavbar.tsx
@@ -2,6 +2,7 @@ import brandName from "@/constant/BrandName";
 import { useState } from "react";
 import { FaRegCircleUser } from "react-icons/fa6";
 import UserInfo from "../User/UserInfoSmallModel";
+import { Link } from "react-router-dom";
 
 export default function LogoNavbar() {
   const [isUserInfoVisible, setIsUserInfoVisible] = useState(false);
@@ -10,12 +11,12 @@ export default function LogoNavbar() {
     setIsUserInfoVisible((prev) => !prev);
   };
   return (
-    <header className="fixed top-0 w-full h-[80px] drop-shadow-md shadow-input z-[55]">
+    <header className="bg-white fixed top-0 w-full h-[80px] drop-shadow-md shadow-input z-[55]">
       <nav className="max-w-[1500px] w-full h-full flex justify-between items-center mx-auto px-3">
         <div className="flex items-center">
-          <span className="text-2xl font-extrabold text-primary">
+          <Link to="/" className="text-2xl font-extrabold text-primary">
             {brandName}
-          </span>
+          </Link>
         </div>
         <div onClick={handleClick}>
           <FaRegCircleUser size={32} className="fill-primary cursor-pointer" />
